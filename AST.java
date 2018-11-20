44e10a53c0070e3dea99249b847d21a7d14cef46
YousefMohsen <yousefmohsen@outlook.dk>
2018-11-20 22:39:16 +0100

    task1: code generation

--- AST.java
+++ AST.java
@@ -3,131 +3,190 @@ import java.util.Map.Entry;
 import java.util.List;
 import java.util.ArrayList;
 
-class faux{ // collection of non-OO auxiliary functions (currently just error)
-    public static void error(String msg){
-	System.err.println("Interpreter error: "+msg);
-	System.exit(-1);
+class faux { // collection of non-OO auxiliary functions (currently just error)
+    public static void error(String msg) {
+        System.err.println("Interpreter error: " + msg);
+        System.exit(-1);
     }
 }
 
-abstract class AST{
+abstract class AST {
 }
 
-class Start extends AST{
+class Start extends AST {
     public List<TokenDef> tokendefs;
     public List<DataTypeDef> datatypedefs;
 
-    Start(List<TokenDef> tokendefs, List<DataTypeDef> datatypedefs){
-	this.tokendefs=tokendefs;
-	this.datatypedefs=datatypedefs;
+    Start(List<TokenDef> tokendefs, List<DataTypeDef> datatypedefs) {
+        this.tokendefs = tokendefs;
+        this.datatypedefs = datatypedefs;
     }
-
-
 
 
     public String compile() {
         String listString = "";
 
-        for (DataTypeDef s : this.datatypedefs)
-        {
+        for (DataTypeDef s : this.datatypedefs) {
             listString += s.compile() + "\t";
         }
-        return "Xavi Start"+listString;
+        return "Xavi Start" + listString;
     }
 }
 
-class TokenDef extends AST{
+class TokenDef extends AST {
     public String tokenname;// NUM and ID
     public String ANTLRCODE;//#: ('0'..'9')+ ('.'('0'..'9')+)? ;#   #: ('A'..'Z'|'a'..'z'|'_')+ ;     #
-    TokenDef(String tokenname, String ANTLRCODE){
-	this.tokenname=tokenname;
-	this.ANTLRCODE=ANTLRCODE;
+
+    TokenDef(String tokenname, String ANTLRCODE) {
+        this.tokenname = tokenname;
+        this.ANTLRCODE = ANTLRCODE;
     }
+
     public String compile() {
         return this.ANTLRCODE;
     }
 }
 
-class DataTypeDef extends AST{
+class DataTypeDef extends AST {
     public String dataTypeName; //exp
     public List<Alternative> alternatives;
 
-    DataTypeDef(String dataTypeName, List<Alternative> alternatives){
-	this.dataTypeName=dataTypeName;
-	this.alternatives=alternatives;
+    DataTypeDef(String dataTypeName, List<Alternative> alternatives) {
+        this.dataTypeName = dataTypeName;
+        this.alternatives = alternatives;
     }
-    public String compile() {
-        String listString = "";
 
-        for (Alternative s : this.alternatives)
-        {
-            listString += s.compile() + "\t";
+    public String compile() {
+        String listString = "abstract class " + this.dataTypeName + "{};\n";
+        System.out.println("This is where Thiago is supposed to ");
+
+
+        for (Alternative a : this.alternatives) {
+            //System.out.println("ASyssOut"+a.compile(this.dataTypeName));
+
+            listString += a.compile(this.dataTypeName);
         }
-        return " -  "+listString;
+        return " " + listString;
     }
 }
 
-class Alternative extends AST{
+class Alternative extends AST {
     public String constructor;  // Constant -  Variable  -  Mult     -  Add
     public List<Argument> arguments; // Type: NUM, name: -  Type: ID, name: name  -  Type: expr, name: e1
     public List<Token> tokens;// det der står efter :
-    Alternative(String constructor, List<Argument> arguments,  List<Token> tokens){
-	this.constructor=constructor;
-	this.arguments=arguments;
-	this.tokens=tokens;
+
+    Alternative(String constructor, List<Argument> arguments, List<Token> tokens) {
+        this.constructor = constructor;
+        this.arguments = arguments;
+        this.tokens = tokens;
     }
 
 
+    String compileConstructor(){
+        String constInit = "";
+
+        String argsCompiled = "";
+        int index = 1;
+
+        for (Argument a : this.arguments) {
+            String addComma = index == this.arguments.size() ? "" : ",";
+
+            argsCompiled += a.compileArguments() + addComma;
+            index++;
+
+            constInit += a.compileConstructorArguments()+";\n";
+        }
+
+        return  ""+
+                this.constructor +
+                "  ( " + argsCompiled + "){\n" +
+                constInit+
+                "  }\n" ;
+    }
 
 
+    public String compile(String extendsClass) {
+        String variables = "";
+        String extendsString = (extendsClass == null) ? "" : " extends " + extendsClass;
 
-    public String compile() {
+        String compiledConstructer = this.compileConstructor();
+        for (Argument a : this.arguments) {
+            variables+=a.compileVariables();
 
-        String listString = "";
-
-        for (Token s : this.tokens)
-        {
-            listString += s.compile() + "\t";
         }
 
 
 
-        return " - "+listString;
+
+
+        return "" + "class " + this.constructor + extendsString + "{\n" +
+                variables+
+                compiledConstructer+
+
+                "  public String toString(){\n" +
+                "    return \"\"+v;\n" +
+                "  }\n" +
+                "}\n";
     }
 }
 
-class Argument extends AST{
+class Argument extends AST {
     public String type;//expr
     public String name;//e1 or e2
-    Argument(String type, String name){this.type=type; this.name=name;}
-    public String compile() {
-        return " Type: "+type +", name: "+name;
+
+    Argument(String type, String name) {
+        this.type = type;
+        this.name = name;
     }
+
+    public String compileVariables() {
+        return "public " + type + " " + name+";\n";
+    }
+    public String compileArguments() {
+        return "" + type + " " + name;
+    }
+
+    public String compileConstructorArguments() {
+        return "this." + name + " = " + name;
+    }
+
 }
 
-abstract class Token extends AST{
+abstract class Token extends AST {
 
     public String compile() {
         return "token: ";
     }
 }
 
-class Nonterminal extends Token{//v,name,e1,e2
+class Nonterminal extends Token {//v,name,e1,e2
     public String name;
-    Nonterminal(String name){this.name=name;}
+
+    Nonterminal(String name) {
+        this.name = name;
+    }
 
     @Override
     public String compile() {
-        return "Nonterminal: "+this.name;
+        return "Nonterminal: " + this.name;
     }
 }
 
-class Terminal extends Token{// '(' , '*' , '+', ')'
+class Terminal extends Token {// '(' , '*' , '+', ')'
     public String token;
-    Terminal(String token){this.token=token;}
-@Override
+
+    Terminal(String token) {
+        this.token = token;
+    }
+
+    @Override
     public String compile() {
-        return "Terminal: "+this.token;
+        return "Terminal: " + this.token;
     }
 }
 
+/*
+- variable declaration
+- toString
+-
+* */
--- coco_output.java
+++ coco_output.java
@@ -1,2 +1,44 @@
+This is where Thiago is supposed to 
 This is where Messi is supposed to be
-Xavi Start -   - Nonterminal: v		 - Nonterminal: name		 - Terminal: '('	Nonterminal: e1	Terminal: '*'	Nonterminal: e2	Terminal: ')'		 - Terminal: '('	Nonterminal: e1	Terminal: '+'	Nonterminal: e2	Terminal: ')'			
+Xavi Start abstract class expr{};
+class Constant extends expr{
+public NUM v;
+Constant  ( NUM v){
+this.v = v;
+  }
+  public String toString(){
+    return ""+v;
+  }
+}
+class Variable extends expr{
+public ID name;
+Variable  ( ID name){
+this.name = name;
+  }
+  public String toString(){
+    return ""+v;
+  }
+}
+class Mult extends expr{
+public expr e1;
+public expr e2;
+Mult  ( expr e1,expr e2){
+this.e1 = e1;
+this.e2 = e2;
+  }
+  public String toString(){
+    return ""+v;
+  }
+}
+class Add extends expr{
+public expr e1;
+public expr e2;
+Add  ( expr e1,expr e2){
+this.e1 = e1;
+this.e2 = e2;
+  }
+  public String toString(){
+    return ""+v;
+  }
+}
+	
